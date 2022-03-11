72a0bb2c9f3d9dded3e3da1b97d1407aea5878bc
Shreshsuman <76208040+Shreshsuman@users.noreply.github.com>
2022-03-11 17:01:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function identifySound() {
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier = ml5.soundClassifier('',modelReady);
+
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
